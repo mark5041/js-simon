758cfc87e1793137ea90124f92d81549746e2fbd
mark5041 <mark.beloso5041@gmail.com>
2021-12-03 16:18:51 +0100

    completo

--- js/script.js
+++ js/script.js
@@ -1,160 +1,66 @@
-// funzione per ricavare un array contenente i numeri bomba
-function blacklist(num, max_mines)
-{
-    let myArray = [];
-    let max = num + 1;
-    let min = 1;
-    let i = 0;
-    let row = 1;
-    let minesInrow = 0;
-    let number;
-    while(myArray.length < num * max_mines)
+// Visualizzare in pagina 5 numeri casuali poi fateli sparire.
+// Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+function generate_Number()
+{   
+    let Array = [];
+    for(let i = 0; i < 5; i++)
     {
-        // ogni riga della griglia può contenere un massimo di mine
-        if(minesInrow < max_mines)
+        let num = Math.floor(Math.random() * 100) + 1
+        if(!Array.includes(num))
         {
-            minesInrow++;
-            do
+            Array.push(num);
+        }
+        else
+        {
+            i--;
+        }
+    }
+    return Array;
+}
+
+function ArrayToString(array)
+{
+    let string;
+    for(let i = 0; i < array.length; i++)
+    {
+        if(i == 0)
+        {
+            string = array[i] + ' ';
+        }
+        else
+        {
+            string += ' ' + array[i];
+        }
+    }
+    return string;
+}
+
+let array_num = generate_Number();
+let string_num = ArrayToString(array_num);
+let num_box = document.querySelector(".game-over");
+
+num_box.innerHTML = `<h1> ${string_num} </h1>`;
+
+setTimeout(function(){
+    num_box.classList.add("d-none");
+    setTimeout(function(){
+        let error = false;
+        let i = 0;
+        while( i < 5 && error == false)
+        {
+            let user_num = parseInt(prompt("inserisci un numero"));
+            if(!array_num.includes(user_num))
             {
-                number = Math.floor(Math.random() * (max - min)) + min;
+                error = true;
+                alert("hai perso");
             }
-            while(myArray.includes(number))
-            myArray[i] = number;
+            else
+            {
+                i++;
+            }
         }
-        // se il numero massimo di mine viene raggiunto si resetta il contatore e si procede con la riga successiva
-        else    if(minesInrow == max_mines)
-                {
-                    minesInrow = 0;
-                    i--;
-                    max += num;
-                    min += num;
-                    row++;
-                }
-        i++;
-    }
-    // per vedere i numeri bomba in ordine crescente
-    console.log(myArray.sort((a, b) => {
-        if (a > b)
-            return 1;
-        if (a < b)
-            return -1;
-        return 0;
-    }));
-
-    return myArray;
-}
-
-function columnNumber(array_number, dark_list, index)
-{
-    let i = index;
-    if(i > 1)
-    {
-    }
-}
-
-
-/** GAME START */
-
-let diff_selector = document.querySelector(".my-btn");
-
-diff_selector.addEventListener('click', 
-    function()
-    {
-        console.clear();
-        let game_over = document.querySelector(".game-over")
-
-        // reset generale
-        let row = document.querySelector(".row");
-        row.classList.remove("opacity-50");
-        game_over.classList.add("d-none");
-        row.innerHTML = '';
-
-        let diff = document.querySelector(".my-selection");
-        let col;
-        let mines = 4;
-        let css_paper = document.documentElement.style;
-
-        // scelgo il numero di box e di mine per riga in base alla difficolta
-        switch(diff.value)
-        {
-            case '1':
-                col = 10;
-            break;
-            case '2':
-                col = 9;
-            break;
-            case '3':
-                col = 7;
-            break;
-        }
-        css_paper.setProperty('--size', col);
-
-        //  Genero l'Array con le mine
-        let list = blacklist(col, mines);
-        // Creo la griglia
-        let tot_box = col * col;
-        for(let i = 1; i <= tot_box; i++)
-        {
-            let box = document.createElement("div");
-            box.classList.add("box", "dynamic");
-            box.append(i);
-            row.append(box);
-
-            box.addEventListener('click', 
-                function click_trigger()
-                {
-                    let resutl = document.querySelector(".game-over > h1");
-
-                    // Seleziono il numero di box corretti già cliccati
-                    let score = document.querySelectorAll(".box.bg-green");
-                    let score_box = document.querySelector(".score");
-                    if(!list.includes(parseInt(box.innerText)) && box.classList.contains("dynamic"))
-                    {
-                        this.classList.add("bg-green");
-
-                        // Aggiorno il numero di box corretti già cliccati
-                        score = document.querySelectorAll(".box.bg-green");
-                        // Se abbiamo selezionato tutti i box corretti uscirà la schermata di vittoria
-                        if(score.length == (col * col) - col * mines)
-                        {
-                            row.classList.add("opacity-50");
-                            game_over.classList.remove("d-none");
-                            resutl.innerHTML = '';
-                            resutl.innerHTML = 'YOU WIN';
-                            score_box.innerHTML = score.length;
-                        }
-                    }
-                    else
-                    {
-                        let box_created = document.querySelectorAll(".box.dynamic");
-                        let bombs_explded = document.querySelectorAll(".box.bg-red");
-                        let max_attemps = 3;
-                        if(bombs_explded.length < max_attemps - 1)
-                        {
-                            this.classList.add("bg-red");
-                        }
-                        else
-                        {
-                            for(let i = 0; i < box_created.length; i++)
-                            {
-                                if(list.includes(parseInt(box_created[i].innerText)))
-                                {
-                                    // Rendo visibili tutte le mine
-                                    box_created[i].classList.add("bg-red");
-                                }
-                                // Rendo non cliccabili tutti i box
-                                box_created[i].removeEventListener("click", click_trigger);
-                            }
-                            // Schermata del Game Over
-                            row.classList.add("opacity-50");
-                            game_over.classList.remove("d-none");
-                            score_box.innerHTML = score.length;    
-                        }
-                    }
-                    this.removeEventListener("click", click_trigger);
-                    console.log("click");
-                }
-            );
-        }
-    }
-);
+    }, 100)
+}, 10000)
