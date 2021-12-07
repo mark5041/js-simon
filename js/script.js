c7784959599a8ac9c3700e3a02d495de72382f33
mark5041 <mark.beloso5041@gmail.com>
2021-12-07 13:16:34 +0100

    bonus in progress

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function generate_Number()
     let Array = [];
     for(let i = 0; i < 5; i++)
     {
-        let num = Math.floor(Math.random() * 100) + 1
+        let num = Math.floor(Math.random() * 20) + 1
         if(!Array.includes(num))
         {
             Array.push(num);
@@ -18,6 +18,14 @@ function generate_Number()
             i--;
         }
     }
+    
+    Array.sort((a, b) => {
+        if (a > b)
+            return 1;
+        if (a < b)
+            return -1;
+        return 0;
+    });
     console.log(Array);
     return Array;
 }
@@ -41,54 +49,8 @@ function ArrayToString(array)
 
 
 
-function grid(index, array)
-{
-    let row = document.querySelector(".row");
-    let box = document.createElement("div");
-    let string_box = document.querySelector(".game-over");
-    let string = document.querySelector(".game-over > h1");
-    box.classList.add("box");
-    box.append(index);
-    row.append(box);
-
-    let attemps = 3;
-    let result;
-    box.addEventListener("click", 
-    function click_trigger()
-    {
-        if(!array.includes(parseInt(box.innerText)))
-        {
-            box.classList.add("bg-red");
-            let wrong_box = document.querySelectorAll(".box.bg-red");
-            if(wrong_box.length == attemps)
-            {
-                row.classList.add("opacity-50")
-                string_box.classList.remove("d-none");
-                string.innerText='';
-                string.innerText='hai perso';
-            }
-        }
-        else
-        {
-            box.classList.add("bg-green");
-            let right_box = document.querySelectorAll(".box.bg-green");
-            if(right_box.length == 5)
-            {
-                row.classList.add("opacity-50");
-                string_box.classList.remove("d-none");
-                string.innerText='';
-                string.innerText='hai vinto';
-            }
-        }
-    });
-
-    
-}
-
-
-
-
 let array_num = generate_Number();
+console.log(array_num);
 let string_num = ArrayToString(array_num);
 let num_box = document.querySelector(".game-over");
 let row = document.querySelector(".row");
@@ -97,25 +59,47 @@ num_box.innerHTML = `<h1> ${string_num} </h1>`;
 
 setTimeout(function(){
     num_box.classList.add("d-none");
-    row.classList.remove("d-none");
     setTimeout(function(){
-        
-        
-        for(let i = 0; i < 100; i++)
+        let user_numbers = [];
+        let button = document.querySelector(".send")
+        let i = 0;
+        let insert = document.querySelector(".input-box");
+        insert.classList.remove("d-none");
+        button.addEventListener("click", 
+        function()
         {
-            grid(i, array_num);
-        }
-
-        let result = document.querySelector(".game-over > h1").innerText;
-
-        if(result == "win" || result == "lose")
-        {
-            let all_box = document.querySelectorAll(".box");
-            for(let i= 0; i <= all_box.length; i++)
+            let user_number = parseInt(document.querySelector(".form-control").value); 
+            
+            if(i < 5)
             {
-                all_box[i].removeEventListener("click", click_trigger);
+                if(user_number < 1 || user_number > 20)
+                {
+                    alert("il numero nono rientra nel range");
+                    i--;
+                }
+                else
+                {
+                    if(user_numbers.includes(user_number))
+                    {
+                        alert("numero già inserito");
+                        i--;
+                    }
+                    else
+                    {
+                        user_numbers.push(user_number);
+                        if(!array_num.includes(user_number))
+                        {
+                            alert("hai perso");
+                        }
+                        else    if(user_numbers.length == array_num.length)
+                                {
+                                    alert("hai vinto");
+                                }
+                        
+                    }
+                }
             }
-        }
+        });
 
     }, 100)
 }, 2000)
